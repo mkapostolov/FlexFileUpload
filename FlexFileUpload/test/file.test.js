637158f487d4014eba4817ac485a98113a0913ef
mkapostolov <martin.apostolov@progress.com>
2019-07-11 09:40:08 +0300

    Added Travis and Docker setup

--- FlexFileUpload/test/file.test.js
+++ FlexFileUpload/test/file.test.js
@@ -1,3 +1,10 @@
+const request = require('request-promise-native');
+
 test("should return -1 when the value is not present", () => {
   expect([1, 2, 3].indexOf(4)).toBe(-1);
+});
+
+test("sample request", async () => {
+  const response = await request.post("http://localhost:10001/_command/discover");
+  console.log("response = ", response);
 });
