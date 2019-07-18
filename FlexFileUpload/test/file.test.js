e1f6e3cb9fef40df5219cab492bb250de2c9eb56
mkapostolov <martin.apostolov@progress.com>
2019-07-18 15:08:08 +0300

    Moved Travis related stuff in separete folder and fixed test

--- FlexFileUpload/test/file.test.js
+++ FlexFileUpload/test/file.test.js
@@ -1,10 +1,14 @@
+const kid = process.env.KINVEY_KID;
+const service = process.env.KINVEY_SERVICE_ID;
+const serviceEnv = process.env.KINVEY_SERVICE_ENV_ID;
 const request = require('request-promise-native');
+const url = `https://console.kinvey.com/_api/v3/services/${service}/environments/${serviceEnv}/discover-source-metadata`;
 
 test("should return -1 when the value is not present", () => {
   expect([1, 2, 3].indexOf(4)).toBe(-1);
 });
 
 test("sample request", async () => {
-  const response = await request.post("http://localhost:10001/_command/discover");
+  const response = await request.post(url);
   console.log("response = ", response);
 });
