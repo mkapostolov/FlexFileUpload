e4d0f789c6700ca114333a5e40080a05afb5e2e9
mkapostolov <martin.apostolov@progress.com>
2019-07-19 11:07:02 +0300

    Debugging deploy script fail

--- Travis/deploy_to_test_env.sh
+++ Travis/deploy_to_test_env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -v
+echo $PWD
+ls -al
 
 export PACKAGE_VERSION=$(node -p "require('./package.json').version")
 kinvey flex deploy --service ${KINVEY_SERVICE_ID} --env ${KINVEY_SERVICE_ENV_NAME}
