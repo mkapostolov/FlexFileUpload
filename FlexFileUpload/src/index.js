5512e17dc7da1ddf0066317e4cbc8c122fe1f8e2
mkapostolov <martin.apostolov@progress.com>
2019-11-05 16:00:14 +0200

    Bumped package.json version again

--- FlexFileUpload/src/index.js
+++ FlexFileUpload/src/index.js
@@ -2,7 +2,7 @@ const sdk = require("kinvey-flex-sdk");
 const handlers = require("./handlers/index");
 
 sdk.service({ host: "0.0.0.0", port: process.env.PORT }, function(err, flex) {
-  console.log(`===> Flex Service started (SDK v${flex.version})`);
+  console.log(`===> Flex Service started (SDK v${flex.version}).`);
 
   for (var funcName in handlers) {
     flex.functions.register(funcName, handlers[funcName]);
